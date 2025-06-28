056973f8f038cd4f8f393e2e198e3e258f468bbb
Advait-Bankar <advaitbankar45@gmail.com>
2025-06-28 22:28:59 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,29 @@
+# 1st Assignment
+
+n1 = input("Select a number: ")
+n1 = float(n1)
+
+n2 = input("Select another number: ")
+n2 = float(n2)
+
+a = n1 + n2
+s = n1 - n2
+m = n1 * n2
+d = n1 / n2
+
+
+print(f"Addition: {a}")
+print(f"Subtraction: {s}")
+print(f"Multiplication: {m}")
+print(f"Division: {d}")
+
+print("\nThank you very much for trying out this program...")
+
+
+# Personalized Greeting Program
+
+print("\nHi Buddy, What's you name?")
+fn = input("Enter your First Name: ")
+sn = input("Enter your Surname: ")
+
+print(f"Hello, {fn} {sn}! Welcome to Python Programming...")
